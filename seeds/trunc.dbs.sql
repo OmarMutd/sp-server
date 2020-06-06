3af966ae5eb54f4d37e64054eeb9e4876b7ffc27
Omar Muhanna <omarmuhomar@hotmail.com>
2020-06-06 18:14:10 -0500

    tables seeded, truncate fixed

--- seeds/trunc.dbs.sql
+++ seeds/trunc.dbs.sql
@@ -1,1 +1,8 @@
-TRUNCATE pizza;
+TRUNCATE gourmet;
+TRUNCATE pasta;
+TRUNCATE soups_and_salads;
+TRUNCATE subs;
+TRUNCATE wings_and_things;
+TRUNCATE more_for_kids;
+TRUNCATE desserts;
+TRUNCATE beverages;
