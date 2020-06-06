4467cd5669671b3aadb595a3ed3aaaf6f181cd3b
Omar Muhanna <omarmuhomar@hotmail.com>
2020-06-06 17:37:12 -0500

    sql files

--- seeds/seed.all.sql
+++ seeds/seed.all.sql
@@ -1,13 +1,3 @@
-INSERT INTO pizza (title, category, url)
-VALUES
-  ('Personal','Pizza Your Way', ''),
-  ('Small','Pizza Your Way', ''),
-  ('Medium','Pizza Your Way', ''),
-  ('Large','Pizza Your Way',  ''),
-  ('Giant','Pizza Your Way',  ''),
-
-
-
 INSERT INTO gourmet (title, category, ingredients, url)
 VALUES
 (`Margarita`,'gourmet', 'Olive oil, garlic butter, tomatoes, mozzarella & fresh basil.', ''),
@@ -21,25 +11,25 @@ VALUES
 (`Hawaiian (Five-O)`,'gourmet', 'Ham, pineapple, parmesan, provolone & mozzarella cheese.', ''),
 (`Al Capone's (All Meat)`,'gourmet', 'Pepperoni, sausage, ham, beef, parmesan & mozzarella.', ''),
 (`Tony's Favorite (Deluxe)`,'gourmet', 'Pepperoni, ham, sausage, beef, green peppers, onions, black olives, jalapenos, mushrooms Sopranos red sauce, creamy sauce, basil & mozzarella.', ''),
-(`Mediterranean`,'gourmet', 'Tomatoes, mushrooms, spinach, green olives, red onions, Artichoke hearts, feta cheese, olive oil, basil, mozzarella.', ''),
+(`Mediterranean`,'gourmet', 'Tomatoes, mushrooms, spinach, green olives, red onions, Artichoke hearts, feta cheese, olive oil, basil, mozzarella.', '');
 
 
 INSERT INTO pasta (title, category, price, ingredients, url)
 VALUES
-(`Lasagna`,'pasta', '$9.99', 'Cheese, Veggie, or Meat Sauce.', ''),
-(`Cheese or Beef Ravioli`,'pasta', '$9.99', '', ''),
-(`Eggplant, Veal or Chicken Parmigiana`,'pasta', '$9.99', '', ''),
-(`Manicotti`,'pasta', '$9.99', '', ''),
-(`Cannelloni`,'pasta', '$9.99', '', ''),
-(`Spaghetti or Fettuccini`,'pasta', '$9.99', 'Red sauce, Meat Sauce, or Alfredo.', ''),
-(`Fettuccini Chicken Alferdo`,'pasta', '$9.99', '', ''),
-(`Trio Pasta`,'pasta', '$9.99', 'Manicotti, Lasagna, Cheese or Beef Ravioli.', ''),
-(`Baked Ziti`,'pasta', '$9.99', 'Ricotta, Meat Sauce, Red Sauce, or Alfredo.', ''),
-(`Linguini Toscana`,'pasta', '$9.99', 'Grilled chicken, Mushrooms, Spinach, Garlic, Sautéed in a lemon garlic butter sauce.', ''),
-(`Soprano's Murphy Chicken`,'pasta', '$9.99', 'Chicken, Tomatoes, Mushrooms, Green Peppers, Onions & Penne Pasta.', ''),
-(`Chicken Florentines`,'pasta', '$9.99', 'Grilled Chicken, Tomatoes, Spinach, Mushrooms, Garlic, Sautéed with creamy sauce,Topped with mozzarella, Served with Penne Pasta.', ''),
-(`Angel Hair Chicken Mushroom Alfredo`,'pasta', '$9.99', 'Grilled Chicken, Mushrooms, Sautéed in Alfredo sauce, Mozzarella Cheese.', ''),
-(`Chicken Carciofi`,'pasta', '$9.99', 'Grilled chicken, Artichokes, Sautéed with Soprano’s red & creamy sauce.', ''),
+('Lasagna','pasta', '$9.99', 'Cheese, Veggie, or Meat Sauce.', ''),
+('Cheese or Beef Ravioli','pasta', '$9.99', '', ''),
+('Eggplant, Veal or Chicken Parmigiana','pasta', '$9.99', '', ''),
+('Manicotti','pasta', '$9.99', '', ''),
+('Cannelloni','pasta', '$9.99', '', ''),
+('Spaghetti or Fettuccini','pasta', '$9.99', 'Red sauce, Meat Sauce, or Alfredo.', ''),
+('Fettuccini Chicken Alferdo','pasta', '$9.99', '', ''),
+('Trio Pasta','pasta', '$9.99', 'Manicotti, Lasagna, Cheese or Beef Ravioli.', ''),
+('Baked Ziti','pasta', '$9.99', 'Ricotta, Meat Sauce, Red Sauce, or Alfredo.', ''),
+('Linguini Toscana','pasta', '$9.99', 'Grilled chicken, Mushrooms, Spinach, Garlic, Sautéed in a lemon garlic butter sauce.', ''),
+('Sopranos Murphy Chicken','pasta', '$9.99', 'Chicken, Tomatoes, Mushrooms, Green Peppers, Onions & Penne Pasta.', ''),
+('Chicken Florentines','pasta', '$9.99', 'Grilled Chicken, Tomatoes, Spinach, Mushrooms, Garlic, Sautéed with creamy sauce,Topped with mozzarella, Served with Penne Pasta.', ''),
+('Angel Hair Chicken Mushroom Alfredo','pasta', '$9.99', 'Grilled Chicken, Mushrooms, Sautéed in Alfredo sauce, Mozzarella Cheese.', ''),
+('Chicken Carciofi','pasta', '$9.99', 'Grilled chicken, Artichokes, Sautéed with Soprano’s red & creamy sauce.', '');
 
 INSERT INTO soups_and_salads (title, category, price, ingredients, url)
 VALUES
@@ -49,7 +39,7 @@ VALUES
 (`Caesar Salad`,'salad', '$6.99', 'Romaine, Tomato, Black Olives, Cucumber & Croutons.', ''),
 (`Grill Chicken Caesar Salad`,'salad', '$8.99', '', ''),
 (`Crispy Chicken Salad`,'salad', '$7.99', 'Lettuce, Tomato, Cucumbers, Onions, Black Olives, & Croutons.', ''),
-(`Chef Salad`,'salad', '$8.99', 'Lettuce, Tomatoes, Onions, Cucumbers, Green Peppers, Black Olives, Ham, Eggs, Cheese & Croutons', ''),
+(`Chef Salad`,'salad', '$8.99', 'Lettuce, Tomatoes, Onions, Cucumbers, Green Peppers, Black Olives, Ham, Eggs, Cheese & Croutons', '');
 
 INSERT INTO subs (title, category, price, ingredients, url)
 VALUES
@@ -62,7 +52,7 @@ VALUES
 (`*7. - Veggie Sub`,'sub', '$7.99', 'Black olives, Green olives, Tomatoes, Mushrooms, Green Peppers, Onions.', ''),
 (`8. - Eggplant or Chicken or Veal Parmesan`,'sub', '$7.99', `Covered with Soprano's red sauce and mozzarella.`, ''),
 (`9. - Meatball Cheese Sub`,'sub', '$7.99', `Covered with Soprano's red sauce and mozzarella.`, ''),
-(`10. - Philly Cheese Steak Sub`,'sub', '$7.99', 'Green peppers, Onions & Mushrooms.', ''),
+(`10. - Philly Cheese Steak Sub`,'sub', '$7.99', 'Green peppers, Onions & Mushrooms.', '');
 
 INSERT INTO wings_and_things (title, category, url)
 VALUES
@@ -71,7 +61,7 @@ VALUES
 (`Fried Mushrooms`,'things', '', ''),
 (`Bread Sticks`,'things', '', ''),
 (`Sicilian Cheese Squares`,'things', '', ''),
-(`Fries`,'things', '', ''),
+(`Fries`,'things', '', '');
 
 
 
@@ -80,7 +70,7 @@ VALUES
 (`Kids Cheese or Pepperoni Pizza`,'kids', '$4.99', ''),
 (`Kids Lasagna, Spaghetti OR Angel Hair`,'kids', '$4.99', ''),
 (`Chicken Fingers with French Fries`,'kids', '$4.99', ''),
-(`Chicken Nuggets with French Fries`,'kids', '$4.99',''),
+(`Chicken Nuggets with French Fries`,'kids', '$4.99','');
 
 INSERT INTO desserts (title, category, price, url)
 VALUES
@@ -88,16 +78,16 @@ VALUES
 (`New York Cheesecake`,'desserts', '$3.95', ''),
 (`Cannoli (1)`,'desserts', '$2.99', ''),
 (`Rocky Road Brownie`,'desserts', '$4.25', ''),
-(`Tiramisu`,'desserts', '$4.25', 'https://imgur.com/LZsdgPr'),
+(`Tiramisu`,'desserts', '$4.25', 'https://imgur.com/LZsdgPr');
 
 INSERT INTO beverages (title, category, price)
 VALUES
-(`2-Liter`,'beverage', '$2.99'),
-(`6-Pack`,'beverage', '$3.99'),
-(`Single Can`,'beverage', '$1.29'),
-(`Iced Tea`,'beverage', '$1.99'),
-(`Coffee`,'beverage', '$1.50'),
-(`Large Fountain`,'beverage', '$1.99'),
+('2-Liter', 'beverage', '$2.99'),
+('6-Pack', 'beverage', '$3.99'),
+('Single can', 'beverage', '$1.29'),
+('Iced Tea', 'beverage', '$1.99'),
+('Coffee', 'beverage', '$1.50'),
+('Large Fountain', 'beverage', '$1.99');
 
 
 
