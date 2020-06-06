f5a4aab6270a3057d8ba12c8192237400765b923
Omar Muhanna <omarmuhomar@hotmail.com>
2020-06-06 18:11:19 -0500

    syntax errors fixed with double single quotes

--- seeds/seed.all.sql
+++ seeds/seed.all.sql
@@ -1,17 +1,17 @@
 INSERT INTO gourmet (title, category, ingredients, url)
 VALUES
-(`Margarita`,'gourmet', 'Olive oil, garlic butter, tomatoes, mozzarella & fresh basil.', ''),
-(`Polio's Classic`,'gourmet', 'Grilled Chicken, tomatoes, garlic, mozzarella & fresh basil.', ''),
-(`Olivia's Favorite`,'gourmet', 'Grilled chicken, tomatoes, spinach, mozzarella & freshbasil.', ''),
-(`Johnny Boy (Chicken Alfredo)`,'gourmet', 'Grilled Chicken, Alfredo sauce, parmesan & mozzarella.', ''),
-(`Furio's (Supreme)`,'gourmet', 'Pepperoni, sausage, green peppers, onions, mushrooms & mozzarella.', ''),
-(`BBQ Grilled Chicken`,'gourmet', 'Grilled Chicken sauteed in bbq sauce, covered with mozzarella cheese.', ''),
-(`Garden Veggies`,'gourmet', 'Tomatoes, onions, green peppers, mushrooms, black olives, basil & mozzarella.', ''),
-(`Carciofi (Artichoke Deluxe`,'gourmet', 'Artichokes, tomatoes, onions, green peppers, black olives, mozzarella.', ''),
-(`Hawaiian (Five-O)`,'gourmet', 'Ham, pineapple, parmesan, provolone & mozzarella cheese.', ''),
-(`Al Capone's (All Meat)`,'gourmet', 'Pepperoni, sausage, ham, beef, parmesan & mozzarella.', ''),
-(`Tony's Favorite (Deluxe)`,'gourmet', 'Pepperoni, ham, sausage, beef, green peppers, onions, black olives, jalapenos, mushrooms Sopranos red sauce, creamy sauce, basil & mozzarella.', ''),
-(`Mediterranean`,'gourmet', 'Tomatoes, mushrooms, spinach, green olives, red onions, Artichoke hearts, feta cheese, olive oil, basil, mozzarella.', '');
+('Margarita','gourmet', 'Olive oil, garlic butter, tomatoes, mozzarella & fresh basil.', ''),
+('Polio''s Classic','gourmet', 'Grilled Chicken, tomatoes, garlic, mozzarella & fresh basil.', ''),
+('Olivia''s Favorite','gourmet', 'Grilled chicken, tomatoes, spinach, mozzarella & freshbasil.', ''),
+('Johnny Boy (Chicken Alfredo)','gourmet', 'Grilled Chicken, Alfredo sauce, parmesan & mozzarella.', ''),
+('Furio''s (Supreme)','gourmet', 'Pepperoni, sausage, green peppers, onions, mushrooms & mozzarella.', ''),
+('BBQ Grilled Chicken','gourmet', 'Grilled Chicken sauteed in bbq sauce, covered with mozzarella cheese.', ''),
+('Garden Veggies','gourmet', 'Tomatoes, onions, green peppers, mushrooms, black olives, basil & mozzarella.', ''),
+('Carciofi (Artichoke Deluxe','gourmet', 'Artichokes, tomatoes, onions, green peppers, black olives, mozzarella.', ''),
+('Hawaiian (Five-O)','gourmet', 'Ham, pineapple, parmesan, provolone & mozzarella cheese.', ''),
+('Al Capone''s (All Meat)','gourmet', 'Pepperoni, sausage, ham, beef, parmesan & mozzarella.', ''),
+('Tony''s Favorite (Deluxe)','gourmet', 'Pepperoni, ham, sausage, beef, green peppers, onions, black olives, jalapenos, mushrooms Sopranos red sauce, creamy sauce, basil & mozzarella.', ''),
+('Mediterranean','gourmet', 'Tomatoes, mushrooms, spinach, green olives, red onions, Artichoke hearts, feta cheese, olive oil, basil, mozzarella.', '');
 
 
 INSERT INTO pasta (title, category, price, ingredients, url)
@@ -33,52 +33,52 @@ VALUES
 
 INSERT INTO soups_and_salads (title, category, price, ingredients, url)
 VALUES
-(`Soup of the Day - Bowl`,'soup', '$5.95', '', ''),
-(`Garden Salad`,'salad', '$3.50', '', 'Lettuce, Tomatoes, Green Peppers, Onions & croutons.'),
-(`Greek Salad wih Feta Cheese`,'salad', '$7.99', 'Lettuce (Iceberg or Romaine), Cucumbers, Pepperoncini, Black Olives, Tomatoes, Green Olives, Onions & Feta Cheese.' ''),
-(`Caesar Salad`,'salad', '$6.99', 'Romaine, Tomato, Black Olives, Cucumber & Croutons.', ''),
-(`Grill Chicken Caesar Salad`,'salad', '$8.99', '', ''),
-(`Crispy Chicken Salad`,'salad', '$7.99', 'Lettuce, Tomato, Cucumbers, Onions, Black Olives, & Croutons.', ''),
-(`Chef Salad`,'salad', '$8.99', 'Lettuce, Tomatoes, Onions, Cucumbers, Green Peppers, Black Olives, Ham, Eggs, Cheese & Croutons', '');
+('Soup of the Day - Bowl','soup', '$5.95', '', ''),
+('Garden Salad','salad', '$3.50', '', 'Lettuce, Tomatoes, Green Peppers, Onions & croutons.'),
+('Greek Salad wih Feta Cheese','salad', '$7.99', 'Lettuce (Iceberg or Romaine), Cucumbers, Pepperoncini, Black Olives, Tomatoes, Green Olives, Onions & Feta Cheese.', ''),
+('Caesar Salad','salad', '$6.99', 'Romaine, Tomato, Black Olives, Cucumber & Croutons.', ''),
+('Grill Chicken Caesar Salad','salad', '$8.99', '', ''),
+('Crispy Chicken Salad','salad', '$7.99', 'Lettuce, Tomato, Cucumbers, Onions, Black Olives, & Croutons.', ''),
+('Chef Salad','salad', '$8.99', 'Lettuce, Tomatoes, Onions, Cucumbers, Green Peppers, Black Olives, Ham, Eggs, Cheese & Croutons', '');
 
 INSERT INTO subs (title, category, price, ingredients, url)
 VALUES
-(`*1. - Ham & Cheese`,'sub', '$7.99', '', ''),
-(`*2. - Submarine Sub`,'sub', '$7.99', 'Pepperoni, Ham.', ''),
-(`*3. - Deluxe`,'sub', '$7.99', 'Pepperoni, Ham, Mushrooms.', ''),
-(`*4. - Combo Meat`,'sub', '$7.99', 'Ham, Pepperoni, Beef & Sausage.', ''),
-(`*5. - Turkey Sub`,'sub', '$7.99', '', ''),
-(`*6. - Grilled Chicken Sub`,'sub', '$7.99', '', ''),
-(`*7. - Veggie Sub`,'sub', '$7.99', 'Black olives, Green olives, Tomatoes, Mushrooms, Green Peppers, Onions.', ''),
-(`8. - Eggplant or Chicken or Veal Parmesan`,'sub', '$7.99', `Covered with Soprano's red sauce and mozzarella.`, ''),
-(`9. - Meatball Cheese Sub`,'sub', '$7.99', `Covered with Soprano's red sauce and mozzarella.`, ''),
-(`10. - Philly Cheese Steak Sub`,'sub', '$7.99', 'Green peppers, Onions & Mushrooms.', '');
+('*1. - Ham & Cheese','sub', '$7.99', '', ''),
+('*2. - Submarine Sub','sub', '$7.99', 'Pepperoni, Ham.', ''),
+('*3. - Deluxe','sub', '$7.99', 'Pepperoni, Ham, Mushrooms.', ''),
+('*4. - Combo Meat','sub', '$7.99', 'Ham, Pepperoni, Beef & Sausage.', ''),
+('*5. - Turkey Sub','sub', '$7.99', '', ''),
+('*6. - Grilled Chicken Sub','sub', '$7.99', '', ''),
+('*7. - Veggie Sub','sub', '$7.99', 'Black olives, Green olives, Tomatoes, Mushrooms, Green Peppers, Onions.', ''),
+('8. - Eggplant or Chicken or Veal Parmesan','sub', '$7.99', 'Covered with Soprano''s red sauce and mozzarella.', ''),
+('9. - Meatball Cheese Sub','sub', '$7.99', 'Covered with Soprano''s red sauce and mozzarella.', ''),
+('10. - Philly Cheese Steak Sub','sub', '$7.99', 'Green peppers, Onions & Mushrooms.', '');
 
 INSERT INTO wings_and_things (title, category, url)
 VALUES
-(`Hot Mild Lemon Pepper BBQ Teriyaki Hot Garlic`,'wings', '', ''),
-(`Mozzarella Sticks`,'things', '', ''),
-(`Fried Mushrooms`,'things', '', ''),
-(`Bread Sticks`,'things', '', ''),
-(`Sicilian Cheese Squares`,'things', '', ''),
-(`Fries`,'things', '', '');
+('Hot Mild Lemon Pepper BBQ Teriyaki Hot Garlic','wings', ''),
+('Mozzarella Sticks','things', ''),
+('Fried Mushrooms','things', ''),
+('Bread Sticks','things', ''),
+('Sicilian Cheese Squares','things', ''),
+('Fries','things', '');
 
 
 
 INSERT INTO more_for_kids (title, category, price, url)
 VALUES
-(`Kids Cheese or Pepperoni Pizza`,'kids', '$4.99', ''),
-(`Kids Lasagna, Spaghetti OR Angel Hair`,'kids', '$4.99', ''),
-(`Chicken Fingers with French Fries`,'kids', '$4.99', ''),
-(`Chicken Nuggets with French Fries`,'kids', '$4.99','');
+('Kids Cheese or Pepperoni Pizza','kids', '$4.99', ''),
+('Kids Lasagna, Spaghetti OR Angel Hair','kids', '$4.99', ''),
+('Chicken Fingers with French Fries','kids', '$4.99', ''),
+('Chicken Nuggets with French Fries','kids', '$4.99','');
 
 INSERT INTO desserts (title, category, price, url)
 VALUES
-(`Brownie (plain)`,'desserts', '$2.99', ''),
-(`New York Cheesecake`,'desserts', '$3.95', ''),
-(`Cannoli (1)`,'desserts', '$2.99', ''),
-(`Rocky Road Brownie`,'desserts', '$4.25', ''),
-(`Tiramisu`,'desserts', '$4.25', 'https://imgur.com/LZsdgPr');
+('Brownie (plain)','desserts', '$2.99', ''),
+('New York Cheesecake','desserts', '$3.95', ''),
+('Cannoli (1)','desserts', '$2.99', ''),
+('Rocky Road Brownie','desserts', '$4.25', ''),
+('Tiramisu','desserts', '$4.25', 'https://imgur.com/LZsdgPr');
 
 INSERT INTO beverages (title, category, price)
 VALUES
